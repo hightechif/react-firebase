cadd00aa89c172e9c34443b32216f8679e7a360d
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-04-05 20:11:53 +0700

    add file upload route

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import NotFound from './pages/NotFound'
+import Uploader from './pages/Uploader'
 
 const Router = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const Router = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/file-upload" component={Uploader} />
       <Route exact component={NotFound} />
     </Switch>
   </BrowserRouter>
